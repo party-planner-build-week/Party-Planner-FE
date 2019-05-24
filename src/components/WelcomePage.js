c3ca537243a561a82b897eae580531a6297f31d6
taslimy <taslimer@gmail.com>
2019-05-23 21:53:34 -0400

    Heavy UI Work Done, MVP Met, Done With This Week, CORS Fixed, Deployed to Netlify.

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
-import QuickImage from 'react-quick-image';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import QuickImage from "react-quick-image";
 
 class WelcomePage extends Component {
   render() {
@@ -9,17 +9,13 @@ class WelcomePage extends Component {
         <div className="ui teal message">
           <div className="content">
             <p>
-              Hi there! Welcome to Party Planner Application! You can Login
-              or Register below to being.
+              Hi there! Welcome to Party Planner Application! You can Login or
+              Register below to being.
             </p>
           </div>
         </div>
         <div className="ui fluid rounded image">
-          <QuickImage
-            width="1125"
-            height="300"
-            find="birthday"
-          />
+          <QuickImage width="1125" height="300" find="birthday" />
         </div>
         <div className="ui placeholder segment">
           <div className="ui stackable very relaxed two column grid">
